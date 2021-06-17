67bbc10ce62b949331f843495a91c25959f5ca4f
Deyvison <deyvisonuchoa@hotmail.com>
2021-06-17 20:44:31 -0300

    Validação customizada: email não repetido na

--- src/main/java/br/com/project/domain/Cliente.java
+++ src/main/java/br/com/project/domain/Cliente.java
@@ -8,15 +8,13 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import br.com.project.domain.enums.TipoCliente;
 
@@ -28,6 +26,8 @@ public class Cliente implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	
+	@Column(unique = true)
 	private String email;
 	private String cpfOuCnpj;
 	private Long tipo;
--- src/main/java/br/com/project/services/validation/CadastroClienteValidator.java
+++ src/main/java/br/com/project/services/validation/CadastroClienteValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.project.domain.Cliente;
 import br.com.project.domain.enums.TipoCliente;
 import br.com.project.dto.CadastroClienteDTO;
+import br.com.project.repositories.ClienteRepository;
 import br.com.project.resources.exceptions.FieldMessage;
 import br.com.project.services.validation.utils.BR;
 
 public class CadastroClienteValidator implements ConstraintValidator<CadastroCliente, CadastroClienteDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(CadastroCliente ann) {
 	}
@@ -27,6 +34,11 @@ public class CadastroClienteValidator implements ConstraintValidator<CadastroCli
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj()))
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ INVALIDO"));
 
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux != null)
+			list.add(new FieldMessage("email", "email já existente"));
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
