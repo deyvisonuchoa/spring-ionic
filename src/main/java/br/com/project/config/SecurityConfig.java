dc07c87b193b6aa8fb8372212e3249ab02500429
Deyvison <deyvisonuchoa@hotmail.com>
2021-06-21 11:41:17 -0300

    Implementando autorização

--- src/main/java/br/com/project/config/SecurityConfig.java
+++ src/main/java/br/com/project/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.com.project.security.JWTAuthenticationFilter;
+import br.com.project.security.JWTAuthorizationFilter;
 import br.com.project.security.JWTUtil;
 
 @Configuration
@@ -60,6 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
             .anyRequest().authenticated();
         
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
